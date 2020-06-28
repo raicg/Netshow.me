1080814d739586ed13a2e94ff7d0693976a8a635
Raí Gondim <raicg2@gmail.com>
2020-06-28 14:18:02 -0300

    configuring webpacker

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,15 @@
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require.context('../images', true)
+
+require('bootstrap');
+window.jQuery = window.$ = require("jquery")
+window.Popper = require("popper.js")
+
+import "@fortawesome/fontawesome-free/js/all";
+
+import "video.js/dist/video.min";
+
+import '../stylesheets/application'
